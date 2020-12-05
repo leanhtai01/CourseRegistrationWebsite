4b5e6a94e017ddeb994474572229cdd001456cd9
leanhtai01 <leanhtai01@gmail.com>
2020-12-05 17:23:26 +0700

    re-organize files

--- index.php
+++ index.php
@@ -17,7 +17,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Course Registration</title>
-    <link rel="stylesheet" href="./style.css">
+    <link rel="stylesheet" href="./css/style.css">
 </head>
 <body>
     <h1>ĐĂNG KÝ HỌC PHẦN</h1>
